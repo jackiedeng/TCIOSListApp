6642f1a81201c5f4899486dff34786847a88f81c
Jackie <63827088@qq.com>
2018-12-05 17:54:57 +0800

    go

--- TCIOSListApp/TCIOSListApp/ViewController.m
+++ TCIOSListApp/TCIOSListApp/ViewController.m
@@ -7,17 +7,58 @@
 //
 
 #import "ViewController.h"
+#import "AFNetworking.h"
 
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+//http://127.0.0.1:8080/tcserver/list.do
 
+- (void)postRequest{
+  
+    NSString * urlString = @"http://127.0.0.1:8080/tcserver/list.do";
+    AFHTTPSessionManager *manger = [AFHTTPSessionManager manager];
+    
+    manger.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"text/plain",@"text/html",nil];
+    
+    [manger GET:urlString
+     parameters:nil
+       progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+          
+           NSLog(@"%@,%@",task,responseObject);
+           
+           
+       } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+           
+           NSLog(@"error %@",error);
+       }];
+}
+
+- (void)click:(id)sedner{
+    [self postRequest];
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor blackColor];
+    
+    UIButton * button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    
+    [button setTitle:@"click"
+            forState:UIControlStateNormal];
+    
+    [button addTarget:self
+               action:@selector(click:)
+     forControlEvents:  UIControlEventTouchDown];
+    
+    button.frame =CGRectMake(0, 100, 100, 50);
+    
+    [self.view addSubview:button];
     // Do any additional setup after loading the view, typically from a nib.
 }
+
 
 
 @end
