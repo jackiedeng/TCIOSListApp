8a765df03fab387e90eacb04129b1ca531cfedfd
Jackie <63827088@qq.com>
2018-12-05 17:18:43 +0800

    Init

---
+++ TCIOSListApp/TCIOSListApp/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  TCIOSListApp
+//
+//  Created by Jackie on 2018/12/5.
+//  Copyright © 2018 Jackie. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ TCIOSListApp/TCIOSListAppTests/TCIOSListAppTests.m
@@ -0,0 +1,37 @@
+//
+//  TCIOSListAppTests.m
+//  TCIOSListAppTests
+//
+//  Created by Jackie on 2018/12/5.
+//  Copyright © 2018 Jackie. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TCIOSListAppTests : XCTestCase
+
+@end
+
+@implementation TCIOSListAppTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
