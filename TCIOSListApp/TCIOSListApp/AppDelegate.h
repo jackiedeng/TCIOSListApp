4a8b934b97bf6d215499ad74abd033856e1e37e5
dengchao.dc <dengchao.dc@alibaba-inc.com>
2018-12-07 11:09:39 +0800

    fix

---
+++ TCIOSListApp/TCIOSListApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TCIOSListApp
+//
+//  Created by jackiedeng on 2018/12/5.
+//  Copyright © 2018 jackiedeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TCIOSListApp/TCIOSListApp/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  TCIOSListApp
+//
+//  Created by jackiedeng on 2018/12/5.
+//  Copyright © 2018 jackiedeng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
